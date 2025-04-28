a2dac2b2d8d7cb36b333671d95a9a5d5f740859b
Harshal Jain <58989030+harshal54@users.noreply.github.com>
2025-04-28 12:53:47 +0530

    Update hotelgyms.php

--- hotelgyms.php
+++ hotelgyms.php
@@ -1,5 +1,18 @@
-<?php 
-hello Harshal jain
+<?php
+/**
+ * Plugin Name: My Simple Plugin
+ * Description: A very basic WordPress plugin.
+ * Version: 1.0
+ * Author: Harshal Jain
+ */
 
+// Hook into WordPress 'init' action
+add_action('init', 'my_simple_plugin_function');
 
+function my_simple_plugin_function() {
+    // This will add a simple message to the admin footer
+    add_filter('admin_footer_text', function() {
+        return 'Thank you for using My Simple Plugin!';
+    });
+}
 ?>
